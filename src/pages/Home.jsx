c4428a3644f663dbb21ceb4da6f1a820b2839ede
ashardeveloper <80727561+ashardeveloper@users.noreply.github.com>
2023-04-07 03:11:48 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = ({ type }) => {
 
   useEffect(() => {
     const fetchVideos = async () => {
-      const res = await axios.get(`https://youtube-client-o87y.onrender.com/api/videos/${type}`);
+      const res = await axios.get(`/videos/${type}`);
       setVideos(res.data);
     };
     fetchVideos();
